917e7449dd5f628949466aac67f2cea612135e08
taky-devy <tak.dev91@gmail.com>
2026-07-08 15:59:19 +0900

    特徴量追加, score=0.1633

--- notebooks/add_features.py
+++ notebooks/add_features.py
@@ -2,14 +2,14 @@ import numpy as np
 import polars as pl
 from polars import selectors as cs
 
-def _add_over_all_score(df:pl.DataFrame)->pl.DataFrame:
+def _over_all_score(df:pl.DataFrame)->pl.DataFrame:
     new_feat_name = 'OverallScore'
     return df.with_columns(
         (pl.col('OverallQual') + pl.col('OverallCond'))
         .alias(new_feat_name)
     )
     
-def _add_bath_score(df:pl.DataFrame)->pl.DataFrame:
+def _bath_score(df:pl.DataFrame)->pl.DataFrame:
     new_feat_name = 'BathScore' 
     cols = ['BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath']
     weights = np.array([2. , 1.2, 1.0, 0.5])
@@ -17,21 +17,51 @@ def _add_bath_score(df:pl.DataFrame)->pl.DataFrame:
     score = mat @ weights
     return df.with_columns(pl.Series(new_feat_name, score))
 
-def _add_total_flr_sf(df:pl.DataFrame)->pl.DataFrame:
+def _total_flr_sf(df:pl.DataFrame)->pl.DataFrame:
     new_feat_name = 'TotalFlrSF'
     cols = ['1stFlrSF', '2ndFlrSF', 'TotalBsmtSF']
     return df.with_columns(pl.sum_horizontal(cols).alias(new_feat_name))
 
-def _add_is_overall_ge9():
+def _is_overall_ge9(df: pl.DataFrame) -> pl.DataFrame:
     # OverallQual 列が9以上なら1, それ以外0のラベル列
-    new_feat_name = 'is_overall_ge9'
-    pass
+    new_feat_name = 'IsOverAllGE9'
+    return df.with_columns(
+        (pl.col('OverallQual') >= 9).cast(pl.Int8).alias(new_feat_name)
+    )
+
+def _building_age_at_sale(df: pl.DataFrame) -> pl.DataFrame:
+    # YrSold - Max(YearBuilt, YearRemodAdd)  ※ データ不備による負値を0にクリップ
+    new_feat_name = 'BuildingAgeAtSale'
+    return df.with_columns(
+        (pl.col('YrSold') - pl.max_horizontal('YearBuilt', 'YearRemodAdd'))
+        .clip(lower_bound=0)
+        .alias(new_feat_name)
+    )
+
+def _bsmt_above_ratio(df: pl.DataFrame) -> pl.DataFrame:
+    # TotalBsmtSF / (1stFlrSF + 2ndFlrSF)
+    new_feat_name = 'BsmtAboveRatio'
+    return df.with_columns(
+        (pl.col('TotalBsmtSF') / (pl.col('1stFlrSF') + pl.col('2ndFlrSF')))
+        .alias(new_feat_name)
+    )
+
+def _liv_lot_ratio(df: pl.DataFrame) -> pl.DataFrame:
+    # GrLivArea / LotArea
+    new_feat_name = 'LivLotRatio'
+    return df.with_columns(
+        (pl.col('GrLivArea') / pl.col('LotArea')).alias(new_feat_name)
+    )
 
 def add_modified_features(df:pl.DataFrame)->pl.DataFrame:
     functions = [
-        _add_over_all_score,
-        _add_bath_score,
-        _add_total_flr_sf
+        _over_all_score,
+        _bath_score,
+        _total_flr_sf,
+        _is_overall_ge9,
+        _building_age_at_sale,
+        _bsmt_above_ratio,
+        _liv_lot_ratio,
     ]
     
     for f in functions:
